3bd22e5a40461ef00f2443fef17be502ec951441
Kimberley-saka <kimberleysaka@gmail.com>
2025-03-12 12:03:06 +0200

    added navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import { Outlet, Link } from "react-router-dom"
 
 function NavBar() {
   return (
-    <div className='flex h-10 bg-blue-500'>
-      <ul className='list-none'>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Services</li>
-        <li>Register</li>
+    <nav className=' bg-blue-500'>
+      <ul className='flex list-none'>
+        <li>
+          <Link to="/"> Home </Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+          </li>
+        <li>
+          <Link to="/register">Register</Link>
+          </li>
 
       </ul>
-    </div>
+      <Outlet/>
+    </nav>
   )
 }
 
