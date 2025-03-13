1c44f40620b26afcd8397bae558a1497b9dabb5b
Kimberley-saka <kimberleysaka@gmail.com>
2025-03-13 11:05:08 +0200

    added profile

--- src/components/CompanyProfile.jsx
+++ src/components/CompanyProfile.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 function CompanyProfile() {
   return (
-    <div>
-      
+    <div className='grid grid-cols-2 gap-10'>
+      <img src="../imgs/profile.jpg" alt="image" className='h-96' />
+      <div className='flex flex-col text-left gap-y-4'>
+      <h1 className='text-2xl text-extra-bold md:text-5xl text-bold '>Company Profile</h1>
+      <p>A local Malawian company incorporated under the
+        company's registration act. The company focuses on:
+        Carrying out Environmental Impact Assessment, conducting
+        ESMP for projects of different nature, conducting
+        environmental audits, developing project Briefs, conducting
+        reconnaissance Surveys for mineral exploration projects,
+        carrying out geological mapping and mineral exploration. The
+        consultancy also provide services like map making and
+        others
+    </p>
+       <button className='bg-gray-400 h-10 w-32'
+       >Learn More</button>
+      </div>
+  
     </div>
   )
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,19 +1,22 @@
 import NavBar from "../components/NavBar"
 import Hero from "../components/Hero"
+import CompanyProfile from "../components/CompanyProfile"
 
 function landing() {
   return (
     <>
-    <section id="hero" className=" relative h-screen bg-black bg-cover bg-center bg-no-repeat px-20 py-14">
+    <section id="hero" className=" relative h-screen bg-black bg-cover 
+    bg-center bg-no-repeat px-20 py-14">
 
     <NavBar/>
     <Hero/>
-    <img src="../imgs/bg.jpg" alt="hero image"  className="absolute inset-0 w-full h-full object-cover opacity-20"/>
+    <img src="../imgs/bg.jpg" alt="hero image"  className="absolute inset-0
+    w-full h-full object-cover opacity-30"/>
       
     </section>
 
-    <section>
-      profile
+    <section id=" profile" className=" py-24 bg-black text-white">
+      <CompanyProfile/>
     </section>
 
     </>
